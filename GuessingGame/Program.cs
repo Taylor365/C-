159509a461232611d3072afbe358ff301f614216
Taylor365 <carltaylor1988@hotmail.com>
2017-05-19 17:40:33 +0100

    Add files via upload

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            Random r = new Random();
+            int secret = r.Next(1, 21);
+            do
+            {
+                Console.Write("Guess the number: ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > secret)
+                {
+                    Console.WriteLine("Too high my friend. Try Again");
+                }
+                else if (guess < secret)
+                {
+                    Console.WriteLine("Too low my friend. Try Again");
+                }
+
+            } while (secret != guess);
+            Console.WriteLine("Congrats! You guessed the secret number {0}.", secret);
+            Console.ReadLine();
+        }
+    }
+}
